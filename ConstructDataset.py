78de17ee005333abe16333e1ff81b4315bad2cbf
HaochengYe <34099007+HaochengYe@users.noreply.github.com>
2019-12-30 17:57:44 +0800

    update

--- ConstructDataset.py
+++ ConstructDataset.py
@@ -9,7 +9,7 @@ from datetime import datetime
 import pandas as pd
 import numpy as np
 
-# %%
+
 # get the ticker for the S&P 500 company
 sp500 = symbols.get_sp500_symbols()
 ticker_list = []
@@ -18,25 +18,24 @@ for i in range(len(sp500)):
     unicode = sp500[i]['symbol']
     ticker_list.append(str(unicode).strip('\n'))
 
-START = datetime(2001,1,1)
+START = datetime(2001, 1, 1)
 END = datetime(2019, 12, 25)
 # only keeps "adj close" column, delete the rest
 del_col = ['High', 'Low', 'Open', 'Close', 'Volume']
 # master dataset where contain all S&P 500 data
 master = pd.DataFrame(columns=['Date'])
 
-
 # %%
 
 for ticker in ticker_list:
     try:
-        dataset = pdr.get_data_yahoo(symbols = ticker, start = START, end = END)
+        dataset = pdr.get_data_yahoo(symbols=ticker, start=START, end=END)
         # only take data with more than 100 months for testing (as the strategy will rebalance every month)
         if len(dataset) > 2000:
-            dataset.drop(del_col, axis = 1, inplace = True)
+            dataset.drop(del_col, axis=1, inplace=True)
             # rename the individual stock closing price as its ticker
             dataset.columns = [ticker]
-            master = pd.merge(master, dataset, how = 'outer', on = 'Date')
+            master = pd.merge(master, dataset, how='outer', on='Date')
         print(ticker)
     except KeyError:
         print(ticker + " failed!")
