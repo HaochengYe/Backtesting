1381b56b7d84f6ec68c7e33eaa08c19e041aa0e0
Lazarus_Yehc <34099007+HaochengYe@users.noreply.github.com>
2020-04-25 01:24:09 -0400

    1

--- ResNet-CNN.py
+++ ResNet-CNN.py
@@ -106,3 +106,49 @@ class ResNetLayer(nn.Module):
     def forward(self, x):
         x = self.blocks(x)
         return x
+
+
+class ResNetEncoder(nn.Module):
+    def __init__(self, in_channels=3, blocks_sizes=[64, 128, 256, 512], deepths=[1,1,1,1], activation='relu', block=ResNetBottleNeckBlock, *args, **kwargs):
+        super().__init__()
+        self.block_sizes = blocks_sizes
+
+        self.gate = nn.Sequential(
+            nn.Conv2d(in_channels, self.block_sizes[0], kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(self.block_sizes[0]),
+            activation_func(activation),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+        self.in_out_block_sizes = list(zip(blocks_sizes, blocks_sizes[1:]))
+
+        self.blocks = nn.ModuleList([
+            ResNetLayer(blocks_sizes[0], blocks_sizes[0], n=deepths[0], activation=activation, block=block, *args, **kwargs),
+            *[ResNetLayer(in_channels * block.expansion, out_channels, n=n, activation=activation, block=block, *args, **kwargs)
+              for (in_channels, out_channels), n in zip(self.in_out_block_sizes, deepths[1:])
+            ]
+        ])
+
+
+    def forward(self, x):
+        x = self.gate(x)
+        for block in self.blocks:
+            x = block(x)
+
+        return x
+
+class ResNetDecoder(nn.Module):
+    def __init__(self, in_features, n_classes):
+        super().__init__()
+        self.avg = nn.AdaptiveAvgPool2d((1,1))
+        self.decoder = nn.Linear(in_features, n_classes)
+
+    def forward(self, x):
+        x = self.avg(x)
+        x = x.view(x.size(0), -1)
+        x = self.decoder(x)
+        return x
+
+
+
+
