246861065c459e767ecf05038a73583abd651f1e
Lazarus_Yehc <34099007+HaochengYe@users.noreply.github.com>
2020-04-25 00:20:21 -0400

    1

---
+++ ResNet-CNN.py
@@ -0,0 +1,108 @@
+import math as m
+import torch
+from torch.optim import Adam
+from torch import nn
+import torchvision
+from functools import partial
+from dataclasses import dataclass
+from collections import OrderedDict
+
+
+class Conv2dAuto(nn.Conv2d):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.padding = (self.kernel_size[0] // 2, self.kernel_size[1] // 2)
+
+
+conv3x3 = partial(Conv2dAuto, kernel_size=3, bias=False)
+
+
+def activation_func(activation):
+    return nn.ModuleDict([
+        ['relu', nn.ReLU(inplace=True)],
+        ['leaky_relu', nn.LeakyReLU(negative_slope=0.01, inplace=True)],
+        ['selu', nn.SELU(inplace=True)],
+        ['none', nn.Identity()]
+    ])[activation]
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, activation='relu'):
+        super().__init__()
+        self.in_channels, self.out_channels, self.activation = in_channels, out_channels, activation
+        self.blocks = nn.Identity()
+        self.activate = activation_func(activation)
+        self.shortcut = nn.Identity()
+
+    def forward(self, x):
+        residual = x
+        if self.should_apply_shortcut: residual = self.shortcut(x)
+        x = self.blocks(x)
+        x += residual
+        x = self.activate(x)
+
+        return x
+
+    @property
+    def should_apply_shortcut(self):
+        return self.in_channels != self.out_channels
+
+
+class ResNetResidualBlock(ResidualBlock):
+    def __init__(self, in_channels, out_channels, expansion=1, downsampling=1, conv=conv3x3, *args, **kwargs):
+        super().__init__(in_channels, out_channels, *args, **kwargs)
+        self.expansion, self.downsampling, self.conv = expansion, downsampling, conv
+        self.shortcut = nn.Sequential(
+            nn.Conv2d(self.in_channels, self.expanded_channels, kernel_size=1, stride=self.downsampling, bias=False),
+            nn.BatchNorm2d(self.expanded_channels)) if self.should_apply_shortcut else None
+
+    @property
+    def expanded_channels(self):
+        return self.out_channels * self.expansion
+
+    @property
+    def should_apply_shortcut(self):
+        return self.in_channels != self.expanded_channels
+
+
+def conv_bn(in_channels, out_channels, conv, *args, **kwargs):
+    return nn.Sequential(conv(in_channels, out_channels, *args, **kwargs),
+                         nn.BatchNorm2d(out_channels))
+
+
+class ResNetBasicBlock(ResNetResidualBlock):
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, *args, **kwargs):
+        super().__init__(in_channels, out_channels, *args, **kwargs)
+        self.blocks = nn.Sequential(
+            conv_bn(self.in_channels, self.out_channels, conv=self.conv, bias=False, stride=self.downsampling),
+            activation_func(self.activation),
+            conv_bn(self.out_channels, self.expanded_channels, conv=self.conv, bias=False), )
+
+
+class ResNetBottleNeckBlock(ResNetResidualBlock):
+    expansion = 4
+
+    def __init__(self, in_channels, out_channels, *args, **kwargs):
+        super().__init__(in_channels, out_channels, expansion=4, *args, **kwargs)
+        self.blocks = nn.Sequential(
+            conv_bn(self.in_channels, self.out_channels, self.conv, kernel_size=1),
+            activation_func(self.activation),
+            conv_bn(self.out_channels, self.out_channels, self.conv, kernel_size=3, stride=self.downsampling),
+            activation_func(self.activation),
+            conv_bn(self.out_channels, self.expanded_channels, self.conv, kernel_size=1), )
+
+
+class ResNetLayer(nn.Module):
+    def __init__(self, in_channels, out_channels, block=ResNetBottleNeckBlock, n=1, *args, **kwargs):
+        super().__init__()
+        downsampling = 2 if in_channels != out_channels else 1
+        self.blocks = nn.Sequential(
+            block(in_channels, out_channels, *args, **kwargs, downsampling=downsampling),
+            *[block(out_channels * block.expansion,
+                    out_channels, downsampling=1, *args, **kwargs) for _ in range(n - 1)])
+
+    def forward(self, x):
+        x = self.blocks(x)
+        return x
