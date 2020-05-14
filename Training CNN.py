712921f5de4bc26c3a145c517591459cb5c9f931
HaochengYe <34099007+HaochengYe@users.noreply.github.com>
2020-05-14 00:28:28 -0400

    giga

--- Training CNN.py
+++ Training CNN.py
@@ -1,38 +1,19 @@
 import os
-import yfinance as yf
-from datetime import datetime
-import pandas as pd
-import pytz
-import logging
+
 import numpy as np
-import plotly.graph_objects as go
-from PIL import Image
-
-import matplotlib.pyplot as plt
-
-import torchvision.transforms as transforms
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from torch.autograd import Variable
 
-from torch.optim import Adam, SGD
 import gc
 
 from ResNet_CNN import *
 
-import math as m
 import torch
-from torch.nn import Linear, ReLU, Conv1d, Conv2d, Sequential, CrossEntropyLoss, MSELoss, MaxPool1d, MaxPool2d, Dropout, BatchNorm1d, BatchNorm2d
 
 from torch.optim import Adam
 from torch import nn
-import torchvision
-from functools import partial
-from collections import OrderedDict
 
-import torchvision.models as models
-
-from torchsummary import summary
 
 def dataLoader(path):
     loaded = np.load(path)
@@ -82,7 +63,7 @@ def train(epochs):
 
 if __name__ == '__main__':
 
-    ticker_list = os.listdir('CD:/GitHub/Backtesting/images_npy')
+    ticker_list = os.listdir('D:/GitHub/Backtesting/images_npy')
     model = res_conv1(1, 64)
     lr = 0.0001
     optimizer = Adam(model.parameters(), lr=lr)
