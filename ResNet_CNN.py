f7e9ad17399da0698b54dbccd25c07ba9432c435
Lazarus_Yehc <34099007+HaochengYe@users.noreply.github.com>
2020-05-20 18:33:05 -0400

    1

--- ResNet_CNN.py
+++ ResNet_CNN.py
@@ -144,14 +144,8 @@ class ResNetDecoder(nn.Module):
         super().__init__()
         self.avg = nn.AdaptiveAvgPool2d((1, 1))
         self.decoder = nn.ModuleList([
-            nn.Linear(in_features, 100),
-            nn.Dropout2d(0.5),
-            nn.ReLU(),
-            nn.Linear(100, 25),
-            nn.Dropout2d(0.5),
-            nn.ReLU(),
-            nn.Linear(25, 1),
-            nn.Sigmoid()
+            nn.Linear(in_features, n_classes),
+            nn.Softmax()
         ])
 
     def forward(self, x):
@@ -162,15 +156,51 @@ class ResNetDecoder(nn.Module):
         return x
 
 
+class LSTM(nn.Module):
+ 
+    def __init__(self, input_dim, hidden_dim, batch_size=1, output_dim=1,
+                    num_layers=2):
+        super(LSTM, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.batch_size = batch_size
+        self.num_layers = num_layers
+ 
+        # Define the LSTM layer
+        self.lstm = nn.LSTM(self.input_dim, self.hidden_dim, self.num_layers)
+ 
+        # Define the output layer
+        self.linear = nn.Linear(self.hidden_dim, output_dim)
+ 
+    def init_hidden(self):
+        # This is what we'll initialise our hidden state as
+        return (torch.zeros(self.num_layers, self.batch_size, self.hidden_dim),
+                torch.zeros(self.num_layers, self.batch_size, self.hidden_dim))
+ 
+    def forward(self, input):
+        # Forward pass through LSTM layer
+        # shape of lstm_out: [input_size, batch_size, hidden_dim]
+        # shape of self.hidden: (a, b), where a and b both 
+        # have shape (num_layers, batch_size, hidden_dim).
+        lstm_out, self.hidden = self.lstm(input.view(len(input), self.batch_size, -1))
+        
+        # Only take the output from the final timetep
+        # Can pass on the entirety of lstm_out to the next layer if it is a seq2seq prediction
+        y_pred = self.linear(lstm_out[-1].view(self.batch_size, -1))
+        return y_pred.view(-1)
+
+
 class ResNet(nn.Module):
     def __init__(self, in_channels, n_classes, *args, **kwargs):
         super().__init__()
         self.encoder = ResNetEncoder(in_channels, *args, **kwargs)
         self.decoder = ResNetDecoder(self.encoder.blocks[-1].blocks[-1].expanded_channels, n_classes)
+        self.LSTM = LSTM(input_dim=n_classes, hidden_dim=64)
 
     def forward(self, x):
         x = self.encoder(x)
         x = self.decoder(x)
+        x = self.LSTM(x)
 
         return x
 
