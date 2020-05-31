7c99248cc887312eb9a7f2b7cc0fbea363e19bcb
HaochengYe <34099007+HaochengYe@users.noreply.github.com>
2020-05-30 22:02:05 -0400

    backtesting

--- Implementation_1.1.py
+++ Implementation_1.1.py
@@ -10,6 +10,15 @@ sns.set()
 
 class Agent():
 
-    def __init__(self, portfolio, trading_strategies, rebalancing_strategies, cycle, max_holding):
+    def __init__(self, portfolio, data, trading_strategies, rebalancing_strategies, cycle, max_holding):
 
         self.portfolio = portfolio
+        self.data = data
+        self.trading_strategies = trading_strategies
+        self.rebalancing_strategies = rebalancing_strategies
+        self.cycle = cycle
+
+
+    @property
+    def equity(self):
+        
