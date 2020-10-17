4f182f96410aba13c0aea69f1fb4fea19bfef6b5
HaochengYe <34099007+HaochengYe@users.noreply.github.com>
2020-10-17 11:52:39 -0400

    Update LSTM_Prediction_1.py

--- LSTM_Prediction_1.py
+++ LSTM_Prediction_1.py
@@ -160,7 +160,7 @@ ticker_list.remove('SPY')
 _ = int(0)
 result = {}
 
-for tick in ticker_list[0:5]:
+for tick in ticker_list[0:500]:
     original_series = data[tick]
 
     if tick in data.columns:
@@ -257,5 +257,5 @@ for tick in ticker_list[0:5]:
 
 result_dta = pd.DataFrame(result).T
 result_dta.columns = ['PredRet', 'NetProfit', 'GrossProfit', 'Var', 'Sharpe', 'R2']
-result_dta.to_csv('LSTM_Prediction_2.csv')
+result_dta.to_csv('LSTM_Prediction_1.csv')
 
